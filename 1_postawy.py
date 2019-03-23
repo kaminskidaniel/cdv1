6da4ab7b70c3bc649c204d4dcf6ebf5ba9584648
Daniel Kamiński <dkaminski@edu.cdv.pl>
2019-03-23 19:07:06 +0100

    nowy

--- 1_postawy.py
+++ 1_postawy.py
@@ -1,7 +1,65 @@
 print("CDV")
 print(2)
+
 #potega
 potega = 2 ** 10
 print(potega)
+
 tekst = "CDV"
 print(tekst * 2)
+
+#pobieranie danych z klawiatury
+# imie = input()
+imie = "Julia"
+print("Twoje imie: " + imie)
+
+nazwisko = "Nowak"
+print("Twoje dane: " + imie + " " +nazwisko )
+
+dlugosc = len(imie)
+print(dlugosc)
+print(type(dlugosc))
+dlugosc = str(dlugosc)
+print(type(dlugosc))
+print("dlugosc imienia: " + dlugosc)
+
+wiek = "20"
+print("Wiek: " + wiek)
+
+wiekJulia = 20
+print(type(wiekJulia))
+wiekJulia = str(wiekJulia)
+print("Wiek Julii: ",wiekJulia)
+
+nazwisko = "Kowalski"
+pierwszyZnak = nazwisko[0]
+print(pierwszyZnak)
+
+ostatniZnak = nazwisko[len(nazwisko) - 1]
+print(ostatniZnak)
+ostatniZnak = nazwisko[-1]
+print(ostatniZnak)
+
+#Konwersja
+x = "5"
+print(type(x))
+x = int(x)
+print(type(x))
+
+x = float(x)
+print(type(x))
+print(x)
+
+y = 5
+print(type(y))
+y = y / 2
+print(type(y))
+print(y)
+
+nazwisko = "Kowalski"
+print(nazwisko[0])
+print(nazwisko[0:3])
+print(nazwisko[-2])
+print(nazwisko[-2:])
+print(nazwisko[:-2])
+print(nazwisko[:-2:2])
